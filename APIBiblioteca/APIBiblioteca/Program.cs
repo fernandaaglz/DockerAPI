033b1171ed7cda815e1945ec13793a0cf5cf30de
fgonzalez-tenco <fgonzalez@pop-os.localdomain>
2023-08-18 18:19:07 -0600

    Some changes

--- APIBiblioteca/APIBiblioteca/Program.cs
+++ APIBiblioteca/APIBiblioteca/Program.cs
@@ -1,5 +1,4 @@
 using APIBiblioteca.Context;
-using LibraryNetCoreAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -30,37 +29,17 @@ if (app.Environment.IsDevelopment())
 }
 
 //Initial migration
-PrepareDb.Population(app);
+using(var scope = app.Services.CreateScope()){
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDBContext>();
+    Console.WriteLine("Applying initial migration..."); 
+    db.Database.Migrate();
+    Console.WriteLine("Initial migration (database) done!");
+}
 
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
 
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
